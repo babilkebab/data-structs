07eca37b91a17b811461535eea6f0e592c17bf07
babilkebab <101179902+babilkebab@users.noreply.github.com>
2022-03-15 11:24:12 +0100

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -10,23 +10,63 @@ class Stack{
         }
 
         void push(T data){
-            if(ptr == len){
-                cout << "Stack pieno" << endl;
+            if(ptr == len-1){
+                cout << "Full stack" << endl;
                 return;
             }
-            st[ptr+1] = data;
-            ptr++;
+            st[++ptr] = data;
+        }
+
+        void pushmultiple(int cnt, ...){
+            if(cnt > len - (ptr+1)){
+                cout << "Invalid operation" << endl;
+                return;
+            }
+            va_list elems;
+            va_start(elems, cnt);
+            for(int i = 0; i < cnt; i++){
+                this->push(va_arg(elems, T));
+            }
+        }
+
+        void popmultiple(int cnt){
+            if (cnt > ptr+1){
+                cout << "Invalid operation" << endl;
+                return;
+            }
+            for(int i = 0; i < cnt; i++){
+                this->pop();
+            }
         }
 
         void pop(){
             if(ptr == -1){
-                cout << "Stack vuoto" << endl;
+                cout << "Empty stack" << endl;
                 return;
             }
             ptr--;
         }
 
-        void access(){
-            cout << st[ptr] << endl;
+        T access(){
+            if(ptr == -1){
+                cout << "Empty stack" << endl;
+                return 0;
+            }
+            return st[ptr];
         }
+
+        vector<T> accessmultiple(int numelems){
+            vector<T> ret;
+            if(numelems > ptr+1){
+                cout << "Too elements to access" << endl;
+                return ret;
+            }
+            int fptr = ptr;
+            for(int i = 0; i < numelems; i++){
+                ret.push_back(st[fptr]);
+                fptr--;
+            }
+            return ret; 
+        }
+
 };
