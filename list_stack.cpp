6dcd495a17e8cc3c34d010db196d83aefdbb1b32
babilkebab <101179902+babilkebab@users.noreply.github.com>
2022-05-16 19:02:06 +0200

    Update list_stack.cpp

--- list_stack.cpp
+++ list_stack.cpp
@@ -6,7 +6,7 @@ using namespace std;
 template <typename T>
 class ListStack{
     list<T>* liststk;
-    //liststk.front() for SP (here's a ref)
+    //liststk.front() for SP (this is a ref)
     public:
         ListStack(){
             liststk = new list<T>;
@@ -32,17 +32,3 @@ class ListStack{
         }
 
 };
-
-
-
-
-
-int main(){
-    ListStack<int> stk;
-    stk.push(10);
-    stk.push(20);
-    stk.push(30);
-    stk.pop();
-    stk.push(300);
-    cout << stk.getTop() << endl; //300
-}
