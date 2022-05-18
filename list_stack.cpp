fa63a212d4183c201cd7315e87acaad1a4cd6f87
babilkebab <101179902+babilkebab@users.noreply.github.com>
2022-05-18 09:03:09 +0200

    Add files via upload

--- list_stack.cpp
+++ list_stack.cpp
@@ -6,7 +6,7 @@ using namespace std;
 template <typename T>
 class ListStack{
     list<T>* liststk;
-    //liststk.front() for SP (returns a ref)
+    //liststk->front() for SP (returns a ref)
     public:
         ListStack(){
             liststk = new list<T>;
@@ -20,9 +20,12 @@ class ListStack{
             liststk->push_front(data);
         }
 
-        void pop(){
-            if(!liststk->empty())
+        T pop(){
+            if(!liststk->empty()){
+                T ret = getTop();
                 liststk->pop_front();
+                return ret;
+            }
             else
                 cout << "Empty stack!" << endl;
         }
@@ -32,3 +35,17 @@ class ListStack{
         }
 
 };
+
+
+
+
+
+int main(){
+    ListStack<int> stk;
+    stk.push(10);
+    stk.push(20);
+    stk.push(30);
+    stk.pop();
+    stk.push(300);
+    cout << stk.getTop() << endl; //300
+}
